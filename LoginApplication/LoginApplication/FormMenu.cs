8d0e6c76c51784fc775a9465442af225978e5549
TuanPham <tuanpham@bravesoft.vn>
2018-03-08 09:31:52 +0700

    update width gridview to print

--- LoginApplication/LoginApplication/FormMenu.cs
+++ LoginApplication/LoginApplication/FormMenu.cs
@@ -196,11 +196,11 @@ namespace MenuManagerApplication
             height += lgvMenu.ColumnHeadersHeight;
 
             int width = 0;
+          
             foreach (DataGridViewColumn col in lgvMenu.Columns)
             {
-                width += col.Width;
+                    width += col.Width;
             }
-            width += lgvMenu.RowHeadersWidth;
 
             Bitmap bm = new Bitmap(width,height);
             return bm;
